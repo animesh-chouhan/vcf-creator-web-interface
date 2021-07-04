3df8159965cdb3a1a2b087d5a7f943dab8656917
Animesh Singh Chouhan <animeshsingh.iitkgp@gmail.com>
2021-07-04 22:16:52 +0530

    Updated on Sunday 04 July 2021 10:16:52 PM IST

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to repos...\033[0m\n"
+
+# Remove processed csv files
+rm -rf processed/*
+
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Updated on $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+echo $msg
+git commit -m "$msg"
+
+# Push source and build repos.
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+git push origin main
+printf "\033[0;32mDeploying updates to Heroku...\033[0m\n"
+git push heroku main
+
+printf "\033[0;32mDone\033[0m\n"
